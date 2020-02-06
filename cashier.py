e377d27a6b6b04eb63c328957e73c894d693a9e9
Ghada6336 <ghadafahmie6336@gmail.com>
2020-02-07 00:57:20 +0200

    finished

--- cashier.py
+++ cashier.py
@@ -1,5 +1,53 @@
 def main():
-	# write code here
+        Item=""
+        basket=[]
+        while Item !="done" :
+                
+               
+                Item = input("Item (enter \"done\" when finished):")
+                if Item == "done":
+                        break
+                
+                price=float(input("Price:"))
+                quantity=int(input("Quantity:"))
+               
+                case={"Item":Item,"Price":price,"Quantity":quantity}
+                basket.append(case)
+        print(basket)
+               
+        print ("-------------")
+        print ("receipt")
+        print ("-------------")
+        
+        lst=[]
+        for x in basket:
+               print( x["Quantity"]," ",x["Item"]," ",x["Price"],"KD")
+               price= x["Price"]* x["Quantity"]
+               print(price)
+               lst.append(price)
+               
+               
+                       
+                       
+       
+               
+        print ("-------------")
+        print("total price=",sum(lst))
+        
+        
+               
+                       
+                
+         
+
+       
+        
+                        
+                
+                 
+                
+                  
+          
 
 if __name__ == '__main__':
 	main()
